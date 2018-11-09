6e5cdce6d7c6e84290e660c696c3501204dc32c1
Adam <bagrowski.adam@gmail.com>
2018-11-09 08:05:20 +0100

    initial commit

---
+++ src/main/java/com/abag/solrtraining/SolrTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.abag.solrtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrTrainingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/abag/solrtraining/SolrTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abag.solrtraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolrTrainingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
