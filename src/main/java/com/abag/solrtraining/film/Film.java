4765e515cc998329b92997be75c4718e83317f0e
Adam <bagrowski.adam@gmail.com>
2018-11-09 09:41:22 +0100

    refactor

--- src/main/java/com/abag/solrtraining/film/Film.java
+++ src/main/java/com/abag/solrtraining/film/Film.java
@@ -16,11 +16,52 @@ class Film {
     private String name;
 
     @Indexed(name = "directed_by")
-    private String directedBy;
+    private List<String> directedBy;
 
     @Indexed(name = "genre")
     private List<String> genre;
 
     @Indexed(name = "initial_release_date")
     private String releaseDate;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getDirectedBy() {
+        return directedBy;
+    }
+
+    public void setDirectedBy(List<String> directedBy) {
+        this.directedBy = directedBy;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
