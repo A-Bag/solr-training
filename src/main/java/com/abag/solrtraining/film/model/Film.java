fcdf7b7e9df8e089b733e51d23af5cbcfdd18b72
Adam <bagrowski.adam@gmail.com>
2018-11-09 08:30:25 +0100

    add document class 'Film'

---
+++ src/main/java/com/abag/solrtraining/film/model/Film.java
@@ -0,0 +1,26 @@
+package com.abag.solrtraining.film.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+import java.util.List;
+
+@SolrDocument(collection = "films")
+public class Film {
+    @Id
+    @Indexed(name = "id")
+    private String id;
+
+    @Indexed(name = "name")
+    private String name;
+
+    @Indexed(name = "directed_by")
+    private String directedBy;
+
+    @Indexed(name = "genre")
+    private List<String> genre;
+
+    @Indexed(name = "initial_release_date")
+    private String releaseDate;
+}
